1d7c8676e7214589ab02a736ae94974913f45a08
lutherism <xanderjanz@gmail.com>
2015-07-29 03:21:23 -0700

    typo

--- source/javascript/components/lightbox.jsx
+++ source/javascript/components/lightbox.jsx
@@ -55,7 +55,7 @@ var Lightbox = React.createClass({
   renderCloseButton: function renderCloseButton() {
     if (this.props.showCloseButton)
       return (
-        <i className= onClick={this.closeHandler}></a</i>>
+        <i className= onClick={this.closeHandler}></i>
       );
   },
 
