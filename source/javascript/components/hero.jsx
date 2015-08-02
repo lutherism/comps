827db6e66825f288b451090b8656c76129167ba0
lutherism <xanderjanz@gmail.com>
2015-08-01 19:49:56 -0700

    herochild

--- source/javascript/components/hero.jsx
+++ source/javascript/components/hero.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var Hero = React.createClass({
   render: function() {
     return (
-      <div className="hero" />
+      <div className="hero">{this.props.children}</div>
     );
   }
 });
